07ddd00725e9651a7f8b177f00404d8491b3a45d
Mohammad Aboul Ela <rendreal@hotmail.com>
2022-05-20 18:22:18 +0200

    a commit

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -1,13 +1,3 @@
-/*
-TERMINAL COMMANDS
-  dart create myapp
-  cd myapp
-  dart run
-*/
-
 void main(List<String> arguments) {
-  helloFromTerminal(){
-    print("Hello From Terminal");
-  }
-  helloFromTerminal();  // run this code from terminal using dart run
+
 }
---
+++ lib/terminal_commands.dart
@@ -0,0 +1,15 @@
+/*
+TERMINAL COMMANDS
+  dart create myapp
+  cd myapp
+  dart run
+*/
+
+void main(List<String> arguments) {
+  helloFromTerminal(){
+    print("Hello From Terminal $arguments");
+  }
+  helloFromTerminal();  // run this code from terminal using
+  // dart run
+  // dart run learning_dart arg1 arg2
+}
