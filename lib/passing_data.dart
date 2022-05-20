e1955b55e3f843b6f40374787cb5aae66b8adc99
Mohammad Aboul Ela <rendreal@hotmail.com>
2022-05-20 23:41:47 +0200

    a commit

---
+++ lib/passing_data.dart
@@ -0,0 +1,22 @@
+class HardDeskData {
+  Map<String,dynamic>? data; //can be null
+  init() {
+    loadData();
+    return this; // return data in this copy
+  }
+  loadData(){data = {"name":"Ahmed", "id" : 13, "age" : 23, "male":true};
+  }
+}
+class GetDataFromDesk {
+  late Map<String,dynamic> myData; //can't be null
+  var defaultData = {"name":"n/a", "id" : "n/a", "age" : "n/a", "male":"n/a"};
+  init() {
+    var loading = HardDeskData().init().data ?? defaultData;
+    myData = loading;
+    return this;
+  }
+}
+void main(){
+  var loadData = GetDataFromDesk().init();
+  print(loadData.myData["name"]);
+}
