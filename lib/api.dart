10d66e81ea27a8e924671941cb1db3d45da302f7
Mohammad Aboul Ela <rendreal@hotmail.com>
2022-05-21 09:53:43 +0200

    a commit

--- lib/api.dart
+++ lib/api.dart
@@ -1,10 +1,10 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 Future<http.Response> fetchData(String path) {
-return http.get(Uri.parse(path));
-}
+return http.get(Uri.parse(path));}
+
 int ayaNumber = 15;
 String ayaPath ="http://api.alquran.cloud/v1/ayah/$ayaNumber/ar";
 String audioPath = "http://api.alquran.cloud/v1/ayah/$ayaNumber/ar.alafasy";
